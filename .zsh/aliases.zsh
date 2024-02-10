ec63b3498d3dd7a30522733ed47848ac315bfffe
Williams Bussat <williams@wbussat.fr>
2024-02-10 01:11:13 +0100

    fix: :bug: switch exa to eza

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -5,6 +5,6 @@ alias code="open -a 'Visual Studio Code'"
 
 alias gprj="cd $HOME/Projects"
 
-alias ls="exa --icons --group-directories-first"
-alias ll="exa --icons --group-directories-first -l"
+alias ls="eza --icons --group-directories-first"
+alias ll="eza --icons --group-directories-first -l"
 alias grep='grep --color'
